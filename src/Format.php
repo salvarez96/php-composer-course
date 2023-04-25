b66cb49510c9bfa94ca1a2b67a701262c60b544c
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-04-25 00:35:55 -0500

    Clase 10: creando un paquete para entender Composer

---
+++ src/Format.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Text;
+
+class Format
+{
+  public static function toUpper(string $text)
+  {
+    return strtoupper($text);
+  }
+}
---
+++ src/helpers.php
@@ -0,0 +1,6 @@
+<?php
+
+function toUpper(string $text)
+{
+  return Text\Format::toUpper($text);
+}
